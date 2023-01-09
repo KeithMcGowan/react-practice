5f7c5155227b751a33b964770fef098f6b2ed9ed
Keith <keithrichardmcgowan@gmail.com>
2023-01-09 16:52:20 -0500

    Rendering Lists & Conditional Content; Styling React Components; Debugging React Apps; Time to Practice: A Complete Practice Project.

--- expense-tracker/src/components/Expenses/Expenses.js
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -3,12 +3,12 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState(new Date().getFullYear());
+  const [filteredYear, setFilteredYear] = useState((new Date().getFullYear()).toString());
 
   const filterChangeHandler = (selectedYear) => {
-    console.log(selectedYear);
     setFilteredYear(selectedYear);
   };
 
@@ -22,6 +22,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
